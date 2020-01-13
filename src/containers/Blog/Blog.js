3f16255836abec3c936d33fb8c413817d8b851c0
MATRIX\Kamil <tomala.kamil@gmail.com>
2020-01-13 22:30:03 +0100

    Refactorign + add Posts containers

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,43 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import Post from "../../components/Post/Post";
+import Posts from "./Posts/Posts";
 import "./Blog.css";
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostID: null
-  };
-
-  componentDidMount() {
-    axios.get("/posts").then(response => {
-      const posts = response.data.slice(0, 4);
-      const updatePosts = posts.map(post => {
-        return {
-          ...post,
-          author: "Kamil"
-        };
-      });
-      this.setState({ posts: updatePosts });
-      //   console.log(response);
-    });
-  }
-
-  postSelectedHandler = id => {
-    this.setState({ selectedPostID: id });
-  };
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      );
-    });
     return (
       <div className="Blog">
         <header>
@@ -50,7 +18,7 @@ class Blog extends Component {
             </li>
           </ul>
         </header>
-        <section className="Posts">{posts}</section>
+        <Posts />
       </div>
     );
   }
