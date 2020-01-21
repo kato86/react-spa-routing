63efca9ad81f62ad659f1ea071393c33b0f8ae2a
Kamil <tomala.kamil@gmail.com>
2020-01-21 16:38:09 +0100

    React.lazy

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Route, NavLink, Switch } from "react-router-dom";
 
 import Posts from "./Posts/Posts";
-import asyncComponent from "../../hoc/asyncComponent";
-// import NewPost from "./NewPost/NewPost";
-
 import "./Blog.css";
 
-const AsyncNewPost = asyncComponent(() => {
-  return import("./NewPost/NewPost");
-});
+const NewPost = React.lazy(() => import("./NewPost/NewPost"));
+// import asyncComponent from "../../hoc/asyncComponent";
+// import NewPost from "./NewPost/NewPost";
+
+// const AsyncNewPost = asyncComponent(() => {
+//   return import("./NewPost/NewPost");
+// });
 
 class Blog extends Component {
   state = {
@@ -49,9 +50,15 @@ class Blog extends Component {
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
         <Switch>
-          {this.state.auth ? (
-            <Route path="/new-post" component={AsyncNewPost} />
-          ) : null}
+          <Route
+            path="/new-post"
+            render={() => (
+              <Suspense fallback="Loading...">
+                <NewPost />
+              </Suspense>
+            )}
+          />
+
           <Route path="/posts" component={Posts} />
           <Route
             render={() => (
